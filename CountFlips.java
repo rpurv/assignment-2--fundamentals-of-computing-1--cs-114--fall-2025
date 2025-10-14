7371ff9480ce31255df2e7484e568fce9c773e8b
rpurv <purviance@hartford.edu>
2025-10-14 17:36:40 -0400

    Update CountFlips.java

--- CountFlips.java
+++ CountFlips.java
@@ -1,4 +1,29 @@
 public class CountFlips {
   public static void main(String[] args) {
+    // coin object creation
+    Coin coinflip = new Coin();
+
+
+    // variables
+    int headsflipped = 0;
+    int tailsflipped = 0;
+
+
+    // flip coin
+    for(int i = 0; i < 100; i++){
+      coinflip.flip();
+
+      if(coinflip.isHeads()){
+        headsflipped++;
+      }
+      else{
+      tailsflipped++;
+      }
+    }
+
+    
+    // print results
+    System.out.println("Number of Heads: " + headsflipped);
+    System.out.println("Number of Tails: " + tailsflipped);
   }
 }
